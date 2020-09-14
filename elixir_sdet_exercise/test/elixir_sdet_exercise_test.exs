363607474856ec682dbbba417897000e95effe86
jake-rh <jake@readinghorizons.com>
2020-09-14 00:47:03 -0600

    first test with screenshot on failure

--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -1,13 +1,50 @@
 defmodule ElixirSdetExerciseTest do
   # Import helpers
   use Hound.Helpers
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "http://google.com"
-    IO.inspect page_title()
+  @baseUrl "https://www.facebook.com"
+
+  test "click sign up with empty inputs shows validation" do
+    navigate_to @baseUrl
+    createNewAccountBtn = find_element(:id, "u_0_2")
+    click(createNewAccountBtn)
+    signUpBtn = find_element(:xpath, "//*[@id='u_1_s']")
+    click(signUpBtn)
+
+    @lastNameFieldErrorIcon = find_element(:xpath, "//*[@id='u_1_c']/i[1]")
+    #lastNameIconFoundBool = element_displayed?(lastNameFieldErrorIcon)
+    
+    assert(element_displayed?(@lastNameFieldErrorIcon))   
+    
+    catch kind, error ->
+      take_screenshot()
+
+    assert(element_displayed?(@lastNameFieldErrorIcon)) 
+
   end
+
+  test "click sign up with empty inputs shows validation 2" do
+    navigate_to @baseUrl
+    createNewAccountBtn = find_element(:id, "u_0_2")
+    click(createNewAccountBtn)
+    signUpBtn = find_element(:xpath, "//*[@id='u_1_s']")
+    click(signUpBtn)
+
+    @lastNameFieldErrorIcon = find_element(:xpath, "//*[@id='u_1_c']/i[1]")
+    #lastNameIconFoundBool = element_displayed?(lastNameFieldErrorIcon)
+    
+    assert(element_displayed?(@lastNameFieldErrorIcon))   
+    
+    catch kind, error ->
+      take_screenshot()
+
+    assert(element_displayed?(@lastNameFieldErrorIcon)) 
+
+  end
+
+
 end
